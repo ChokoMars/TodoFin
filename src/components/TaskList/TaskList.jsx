b7ce9cd4d878e5ee5d28857539445bbd3bf823ea
ChokoMars <Chokomars-2000@mail.ru>
2023-06-01 18:15:44 +0500

    test

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -8,7 +8,7 @@ import './TaskList.css';
 function TaskList({ todos, onDeleted, onToggleCompleted, onEditItem, onToggleEdit, onSubmit }) {
   const elements = todos.map((item) => {
     const { edit, id, ...itemProps } = item;
-
+    // тестики
     let classNames = '';
 
     if (edit) {
